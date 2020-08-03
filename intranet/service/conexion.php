d6d0420c58cfe0624ca8308d6c4a8d28e86735ad
jhony <jocasillas@espe.edu.ec>
2020-08-03 01:15:24 -0500

    organizacion de archivos

--- intranet/service/conexion.php
+++ intranet/service/conexion.php
@@ -1,15 +1,17 @@
 <?php
-class Connection{
-    function getConnection(){
-        $enlace = mysqli_connect("localhost", "root", "", "proyecto");   
-        if (!$enlace) {
-        echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
-        echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
-        echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
-        echo "<p/>";
-        exit;
-    }
-    return $enlace;
+
+class Connection
+{
+    function getConnection()
+    {
+        $conex = mysqli_connect("127.0.0.1", "root", "admin123", "proyecto");
+        if (!$conex) {
+            echo "<p>Error: No se pudo conectar a MySQL." . PHP_EOL;
+            echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
+            echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
+            echo "</p>";
+            exit;
+        }
+        return $conex;
     }
 }
-?>
