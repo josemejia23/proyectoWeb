65fbb4636206d7c79a9cd4d40764ca5b10cf10b3
ValdiviesoM9 <66709852+ValdiviesoM9@users.noreply.github.com>
2020-08-09 00:05:43 -0500

    matriculas service

--- intranet/services/matriculaService.php
+++ intranet/services/matriculaService.php
@@ -16,6 +16,35 @@ class MatriculaServicios extends MainService
     {
         return $this->conexion->query("SELECT * FROM nivel_educativo ");   
     }
+    
+    function agregarMatricula($cod_periodo_lectivo,$cod_alumno,$cod_nivel_educativo)
+    {
+        $stmt = $this->conexion->prepare("INSERT INTO matricula_periodo (COD_PERIODO_LECTIVO,COD_ALUMNO,COD_NIVEL_EDUCATIVO) 
+                                          VALUES (?,?,?)");
+        $stmt->bind_param('sss',$cod_periodo_lectivo,$cod_alumno,$cod_nivel_educativo);
+        $stmt->execute();
+        $stmt->close();
+    }
+    function periodo()
+    {
+        return $this->conexion->query("SELECT * FROM periodo_lectivo WHERE ESTADO='ACT'");
+    }
+
+    function mostrarPeriodos()
+    {
+        return $this->conexion->query("SELECT * FROM periodo_lectivo ");
+    }
+
+
+    function insertarPeriodo($cod_periodo_lectivo,$estado,$fecha_inicio,$fecha_fin)
+    {
+        $stmt = $this->conexion->prepare("INSERT INTO periodo_lectivo(COD_PERIODO_LECTIVO,ESTADO,FECHA_INICIO,FECHA_FIN) 
+                                          VALUES (?,?,?,?)");
+        $stmt->bind_param('ssss',$cod_periodo_lectivo,$estado,$fecha_inicio,$fecha_fin);
+        $stmt->execute();
+        $stmt->close();
+    }
+
 
 }
 
