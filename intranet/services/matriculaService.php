c488e2df5455cec4862daf5f564fd040fe033004
ValdiviesoM9 <66709852+ValdiviesoM9@users.noreply.github.com>
2020-08-09 00:10:13 -0500

    matriculas service

--- intranet/services/matriculaService.php
+++ intranet/services/matriculaService.php
@@ -44,6 +44,51 @@ class MatriculaServicios extends MainService
         $stmt->execute();
         $stmt->close();
     }
+     //PARALELOS
+     function insertarParalelo($cod_paralelo,$cod_nivel_educativo,$nombre)
+     {
+         $stmt = $this->conexion->prepare("INSERT INTO paralelo (COD_PARALELO,COD_NIVEL_EDUCATIVO,NOMBRE) 
+                                           VALUES (?,?,?)");
+         $stmt->bind_param('sss',$cod_paralelo,$cod_nivel_educativo,$nombre);
+         $stmt->execute();
+         $stmt->close();
+     }
+ 
+     //PERIODO CON EL RESTO DE DATOS
+     
+     function asignaturas($cod_nivel_educativo)
+     {
+         return $this->conexion->query("SELECT * FROM asignatura WHERE  COD_NIVEL_EDUCATIVO='".$cod_nivel_educativo."'");
+     }
+     function encontrarParalelo($cod_nivel_educativo)
+     {
+         return $this->conexion->query("SELECT * FROM paralelo WHERE  COD_NIVEL_EDUCATIVO='".$cod_nivel_educativo."'");
+     }
+     function encontrarAula()
+     {
+         return $this->conexion->query("SELECT * FROM aula");
+     }
+     function encontrarProfesor()
+     {
+         return $this->conexion->query("SELECT persona.COD_PERSONA, persona.NOMBRE, persona.APELLIDO
+                                        FROM persona
+                                        INNER JOIN tipo_persona_persona
+                                        ON persona.COD_PERSONA = tipo_persona_persona.COD_PERSONA
+                                        WHERE tipo_persona_persona.COD_TIPO_PERSONA='PRO'
+                                        ORDER BY persona.APELLIDO");
+     }
+ 
+     //AGREGAR DATOS DEL PERIODO
+     function agregarDatosPeriodo($cod_nivel_educativo,$cod_asignatura,$cod_periodo_lectivo,$cod_paralelo,$cod_docente,$cod_aula)
+     {
+         $stmt = $this->conexion->prepare("INSERT INTO asignatura_periodo (COD_NIVEL_EDUCATIVO,COD_ASIGNATURA,COD_PERIODO_LECTIVO,
+                                                                           COD_PARALELO,COD_DOCENTE,COD_AULA) 
+                                           VALUES (?,?,?,?,?,?)");
+         $stmt->bind_param('ssssss',$cod_nivel_educativo,$cod_asignatura,$cod_periodo_lectivo,$cod_paralelo,$cod_docente,$cod_aula);
+         $stmt->execute();
+         $stmt->close();
+     }
+ 
 
 
 }
