481902193b2e6aa8bed2c6241997d509d248e8bb
jhony <jocasillas@espe.edu.ec>
2020-08-09 00:16:29 -0500

    infraestructura service

--- intranet/services/infraestructuraService.php
+++ intranet/services/infraestructuraService.php
@@ -73,6 +73,41 @@ class infraestructuraServicios extends mainService
         $stmt->execute();
         $stmt->close();
     }
+    function insertarAula($cod_aula, $cod_edificio, $nombre, $capacidad, $tipo, $piso)
+    {
+        $stmt = $this->conexion->prepare("INSERT INTO aula(COD_AULA,COD_EDIFICIO,NOMBRE,CAPACIDAD,TIPO,PISO) 
+                                          VALUES (?,?,?,?,?,?)");
+        $stmt->bind_param('sssisi',$cod_aula, $cod_edificio, $nombre, $capacidad, $tipo, $piso);
+        $stmt->execute();
+        $stmt->close();
+    }
+    function encontrarAula($codigo_aula)
+    {
+        $result = $this->conexion->query("SELECT * FROM aula WHERE COD_AULA='".$codigo_aula."'");
+        if($result->num_rows>0)
+        {
+            return $result->fetch_assoc();
+        }
+        else
+        {
+            return null;
+        }
+    }
+    function modificarAula($cod_aula, $cod_edificio, $nombre, $capacidad, $tipo, $piso,$cod_aula_comparar)
+    {
+        $stmt = $this->conexion->prepare("UPDATE aula SET COD_AULA=?,COD_EDIFICIO=?,NOMBRE=?,CAPACIDAD=?,TIPO=?,PISO=?
+                                          WHERE COD_AULA=?");
+        $stmt->bind_param('sssisis',$cod_aula,$cod_edificio, $nombre, $capacidad, $tipo, $piso,$cod_aula_comparar);
+        $stmt->execute();
+        $stmt->close();
+    }
+    function eliminarAula($codigo_aula)
+    {
+        $stmt = $this->conexion->prepare("DELETE FROM aula WHERE COD_AULA=?");
+        $stmt->bind_param('s',$codigo_aula);
+        $stmt->execute();
+        $stmt->close();
+    }
 
 
    
