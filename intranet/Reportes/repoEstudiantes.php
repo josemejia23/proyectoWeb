ed06575ed1fdad41ce10f7412f30617defbccbba
ValdiviesoM9 <66709852+ValdiviesoM9@users.noreply.github.com>
2020-08-11 15:04:16 -0500

    reportes

--- intranet/Reportes/repoEstudiantes.php
+++ intranet/Reportes/repoEstudiantes.php
@@ -1,51 +1,27 @@
 <?php
-session_start();
-
+	include 'plantilla.php';
+	require 'db.php';
+	
+	$query = "select *from persona";
+	$resultado = $mysqli->query($query);
+	
+	$pdf = new PDF();
+	$pdf->AliasNbPages();
+	$pdf->AddPage();
+	
+	$pdf->SetFillColor(232,232,232);
+	$pdf->SetFont('Arial','B',12);
+	$pdf->Cell(50,6,'Cedula',1,0,'C',1);
+	$pdf->Cell(50,6,'Apellido',1,0,'C',1);
+	$pdf->Cell(50,6,'Nombre',1,1,'C',1);
+	
+	$pdf->SetFont('Arial','',10);
+	
+	while($row = $resultado->fetch_assoc())
+	{
+		$pdf->Cell(50,6,utf8_decode($row['CEDULA']),1,0,'C');
+		$pdf->Cell(50,6,$row['APELLIDO'],1,0,'C');
+		$pdf->Cell(50,6,utf8_decode($row['NOMBRE']),1,1,'C');
+	}
+	$pdf->Output();
 ?>
-
-<?php include("db.php"); ?>
-<?php
-$NOMBRE = '';
-$DIRECCION = '';
-$TELEFONO = '';
-$FECHA_NACIMIENTO = '';
-$accion = "Agregar";
-$COD_PERSONA = "";
-$CEDULA = "";
-$APELLIDO = "";
-$CORREO = "";
-$CORREO_PERSONAL = "";
-
-if (isset($_GET['COD_PERSONA'])) {
-  $result_sede = $conn->query("SELECT * FROM PERSONA WHERE COD_PERSONA=" . $_GET['COD_PERSONA']);
-  if (mysqli_num_rows($result_sede) == 1) {
-    $row = mysqli_fetch_array($result_sede);
-    $COD_PERSONA = $row['COD_PERSONA'];
-    $CEDULA = $row['CEDULA'];
-    $APELLIDO = $row['APELLIDO'];
-    $NOMBRE = $row['NOMBRE'];
-    $DIRECCION = $row['DIRECCION'];
-    $TELEFONO = $row['TELEFONO'];
-    $FECHA_NACIMIENTO = $row['FECHA_NACIMIENTO'];
-    $GENERO = $row['GENERO'];
-    $CORREO_PERSONAL = $row['CORREO_PERSONAL'];
-    $accion = "Modificar";
-  }
-}
-if (isset($_GET['buscar'])) {
-  $resp = '"%' . $_GET['CEDULA'] . '%"';
-  $result_sede = $conn->query("SELECT * FROM PERSONA WHERE CEDULA LIKE" . $resp);
-  if (mysqli_num_rows($result_sede) == 1) {
-    $row = mysqli_fetch_array($result_sede);
-    $COD_PERSONA = $row['COD_PERSONA'];
-    $CEDULA = $row['CEDULA'];
-    $APELLIDO = $row['APELLIDO'];
-    $NOMBRE = $row['NOMBRE'];
-    $DIRECCION = $row['DIRECCION'];
-    $TELEFONO = $row['TELEFONO'];
-    $FECHA_NACIMIENTO = $row['FECHA_NACIMIENTO'];
-    $GENERO = $row['GENERO'];
-    $CORREO_PERSONAL = $row['CORREO_PERSONAL'];
-    $accion = "Modificar";
-  }
-}
